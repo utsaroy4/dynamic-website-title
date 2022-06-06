5e05478302396ad4a6bc477c6064196d104a9ec8
Utsa Roy <49422403+utsaroy4@users.noreply.github.com>
2022-06-06 22:30:53 +0600

    Add files via upload

---
+++ Dynamic Titel .php
@@ -0,0 +1,10 @@
+<!-- for input page  -->
+    <?php  $a ='Home | US Web Software 2020'; ?>
+<!-- this code past on top on "Doctype html" -->
+
+<!-- show on title -->
+    <title><?php echo "$a";?></title>
+
+
+
+<!-- Utsa code collection -->
